b1eb2792901f0c600fb8450acda6db9852c9c80b
codershwaz <wazirshubham@outlook.com>
2026-08-23 23:57:44 +0530

    Changes for railway

--- backend/src/main.py
+++ backend/src/main.py
@@ -6,9 +6,9 @@ app=FastAPI()
 MY_PROJECT = os.environ.get("MY_PROJECT")
 API_KEY = os.environ.get("API_KEY")
 
-if not API_KEY:
-    raise NotImplementedError("API_KEY is not set.")
+# if not API_KEY:
+#     raise NotImplementedError("API_KEY is not set.")
 
-@app.get("/value")
+@app.get("/")
 def read_index():
     return {"text":"Hello from me", "myproject": MY_PROJECT, "apikey": API_KEY}
